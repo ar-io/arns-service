b3311b05bfded91182b82d2d247e85f8fb78fd08
dtfiedler <dylan@ar.io>
2023-04-13 13:35:37 -0500

    fix(lmbd): replace leveldb cache with lmbd
    According to warp - https://academy.warp.cc/docs/sdk/advanced/cache#lmdb-cache
    - lmbd cache is much more performant than leveldb. Our local testing validated this by hitting a single contract with multiple requests at concurrently.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,34 @@
 import Koa, { Next } from 'koa';
 import router from './routes';
-import { WarpFactory } from 'warp-contracts';
+import { defaultCacheOptions, LoggerFactory, LogLevel, WarpFactory } from 'warp-contracts';
 import { KoaContext } from './types';
+import {LmdbCache} from "warp-contracts-lmdb";
 
 const app = new Koa();
 
 // attach warp
 app.use(async (ctx: KoaContext, next: Next) => {
-  const warp = WarpFactory.forMainnet();
+
+  LoggerFactory.INST.logLevel(process.env.LOG_LEVEL as LogLevel ?? 'debug');
+  const warp = WarpFactory.forMainnet()
+  .useStateCache(
+    new LmdbCache({...defaultCacheOptions,})
+  ).useContractCache(
+    // Contract cache
+    new LmdbCache({...defaultCacheOptions}), 
+    // Source cache
+    new LmdbCache({...defaultCacheOptions})
+  );
   ctx.state.warp = warp;
   return next();
 });
 
 app.use(router.routes());
+
+// TODO: add error metrics
+app.on('error', (err) => {
+  console.error(err)
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
