b62bf643f19a95f18512e6639b2252faec0aad04
dtfiedler <dylan@ar.io>
2023-04-13 15:05:05 -0500

    chore(warp): extract warp contract state reads to helper function

---
+++ src/api/warp.ts
@@ -0,0 +1,21 @@
+import { Warp } from "warp-contracts";
+
+const requestMap: Map<string, Promise<any> | undefined> = new Map();
+
+// TODO: we can put this in a interface/class
+export async function getContractState(id: string, warp: Warp){
+    // validate request is new, if not return the existing promise (e.g. barrier synchronization)
+    if(requestMap.get(id)){
+     return await requestMap.get(id);
+    }
+
+    const contract = warp.contract(id);
+    // set cached value for multiple requests during initial promise
+    requestMap.set(id, contract.readState());
+    // await the response
+    const { cachedValue } = await requestMap.get(id);
+    // remove the cached value once it's been retrieved
+    requestMap.delete(id);
+
+    return cachedValue;
+}
--- src/routes/contract.ts
+++ src/routes/contract.ts
@@ -1,34 +1,17 @@
 import { Next } from "koa";
 
 import { KoaContext } from "../types";
-import { Warp } from "warp-contracts";
-
-const requestMap: Map<string, Promise<any> | undefined> = new Map();
+import { getContractState } from "../api/warp";
 
 export async function contractHandler(ctx: KoaContext, next: Next) {
   const { id } = ctx.params;
   try {
-    // validate request is new, if not return the existing promise (e.g. barrier synchronization)
-    if(requestMap.get(id)){
-      const cachedValue = await requestMap.get(id);
-      ctx.body = cachedValue;
-      return next;
-    }
-
-    const warp = ctx.state.warp as Warp;
-    const contract = warp.contract(id);
-    // set cached value for multiple requests during initial promise
-    requestMap.set(id, contract.readState());
-    // await the response
-    const { cachedValue } = await requestMap.get(id);
-    ctx.body = cachedValue.state;
+    const warp = ctx.state.warp;
+    const state = await getContractState(id, warp);
+    ctx.body = state;
   } catch (error){
     ctx.status = 503;
     ctx.body = `Failed to fetch contract: ${id}`
-  } finally {
-    // clean up the promise
-    requestMap.delete(id);
   }
-
   return next;
 }
