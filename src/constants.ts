0b1076d07ddf04734f56af981e6fd5ee6afc5039
dtfiedler <dylan@ar.io>
2023-04-17 14:57:10 -0600

    feat(PE-3368): additional endpoints for balance and record retrieval
    Adds endpoints to retrieve specific fields and values of those fields for a given contract ID. Also adds an endpoint to retrieve the entire field for a given contract field.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
-export const ARNS_CONTRACT_ID_REGEX='([a-zA-Z0-9-_s+]{43}$)'
+export const PDNS_CONTRACT_ID_REGEX='([a-zA-Z0-9-_s+]{43})'
+export const PDNS_CONTRACT_FIELD_REGEX='(balances|records|fees|tiers|ticker|owner|name)'
+export const PDNS_NAME_REGEX='([a-zA-Z0-9-s+]{1,32})'
