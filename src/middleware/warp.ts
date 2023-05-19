4e54fca329edf1a0d7e2ff4616e5edf51a2be868
dtfiedler <dylan@ar.io>
2023-05-19 10:09:15 -0500

    fix(middleware): instantiate middleware outside of handler function to avoid overwriting on each request

--- src/middleware/warp.ts
+++ src/middleware/warp.ts
@@ -6,37 +6,37 @@ import {
   WarpFactory,
   defaultCacheOptions,
 } from "warp-contracts";
+import { arweave } from "./arweave";
 
 LoggerFactory.INST.logLevel(
   (process.env.WARP_LOG_LEVEL as LogLevel) ?? "fatal"
 );
 
-export function warpMiddleware(ctx: KoaContext, next: Next) {
-  const { arweave } = ctx.state;
+/**
+   * TODO: ContractDefinitionsLoader does not have cache implemented when using custom arweave config, so use inMemory for now. Once it is updated, we should revert back to LMDB cache implementation.
+   * 
+   * Reference: https://github.com/warp-contracts/warp/blob/cde7b07f9495f09e998b13d1fe2661b0af0a3b74/src/core/modules/impl/ContractDefinitionLoader.ts#L150-L165
+   * e.g. 
+      const warp = WarpFactory.forMainnet(defaultCacheOptions, true, arweave)
+      .useStateCache(
+          new LmdbCache(defaultCacheOptions)
+      ).useContractCache(
+          // Contract cache
+          new LmdbCache(defaultCacheOptions), 
+          // Source cache
+          new LmdbCache(defaultCacheOptions)
+      );
+*/
+const warp = WarpFactory.forMainnet(
+  {
+    ...defaultCacheOptions,
+    inMemory: true,
+  },
+  true,
+  arweave
+);
 
-  /**
-     * TODO: ContractDefinitionsLoader does not have cache implemented when using custom arweave config, so use inMemory for now. Once it is updated, we should revert back to LMDB cache implementation.
-     * 
-     * Reference: https://github.com/warp-contracts/warp/blob/cde7b07f9495f09e998b13d1fe2661b0af0a3b74/src/core/modules/impl/ContractDefinitionLoader.ts#L150-L165
-     * e.g. 
-        const warp = WarpFactory.forMainnet(defaultCacheOptions, true, arweave)
-        .useStateCache(
-            new LmdbCache(defaultCacheOptions)
-        ).useContractCache(
-            // Contract cache
-            new LmdbCache(defaultCacheOptions), 
-            // Source cache
-            new LmdbCache(defaultCacheOptions)
-        );
-     */
-  const warp = WarpFactory.forMainnet(
-    {
-      ...defaultCacheOptions,
-      inMemory: true,
-    },
-    true,
-    arweave
-  );
+export function warpMiddleware(ctx: KoaContext, next: Next) {
   ctx.state.warp = warp;
   return next();
 }
