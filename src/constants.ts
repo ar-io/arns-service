b8111bbac0d121c9ac05d33abae181c2eb8b04b7
dtfiedler <dylan@ar.io>
2023-06-15 16:39:14 -0500

    fix: increase state load timeout

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const PDNS_CONTRACT_ID_REGEX = "([a-zA-Z0-9-_s+]{43})";
 export const PDNS_CONTRACT_FIELD_REGEX =
   "(balances|records|fees|tiers|ticker|owner|name|controller|approvedANTSourceCodeTxs|settings)";
 export const PDNS_NAME_REGEX = "([a-zA-Z0-9-s+]{1,32})";
-export const EVALUATION_TIMEOUT_MS = 5000;
+export const EVALUATION_TIMEOUT_MS = 10_000; // 10 sec state timeout
 export const allowedContractTypes = ["ant"] as const;
