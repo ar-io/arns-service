5c43de2c0e6301e6ab8a3ccc5b1237d6ac21d749
dtfiedler <dylan@ar.io>
2023-06-15 16:37:55 -0500

    fix(cache): only set cache to two mins on successful response codes

--- src/middleware/headers.ts
+++ src/middleware/headers.ts
@@ -4,7 +4,15 @@ import { KoaContext } from "../types";
 const MAX_AGE_SECONDS = process.env.MAX_AGE_SECONDS ?? 120;
 
 export async function headersMiddleware(ctx: KoaContext, next: Next) {
+  const { logger } = ctx.state;
   await next();
-  // add header at the end of all requests
-  ctx.set("Cache-Control", `max-age=${MAX_AGE_SECONDS}`);
+  if (ctx.status > 299){
+      // don't set cache if we got a bad response
+    logger.debug('Setting cache-control to no-cache.')
+    ctx.set('Cache-Control', 'no-cache');
+  } else {
+    logger.debug(`Setting cache-control to max-age=${MAX_AGE_SECONDS}.`)
+    // add header at the end of all successful requests
+    ctx.set("Cache-Control", `max-age=${MAX_AGE_SECONDS}`);
+  }
 }
