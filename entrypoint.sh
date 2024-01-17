9b595bf3abb24e40629b3048271ff7e6294524bb
dtfiedler <dylan@ar.io>
2024-01-16 17:44:27 -0700

    chore(docker): remove unnecessary line in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+
 # Check and set permissions if necessary
 if [ -d "/usr/src/app/cache" ]; then
-  echo "Setting permissions on /usr/src/app/cache"
-  chown -R node:node /usr/src/app/cache
   chmod -R 755 /usr/src/app/cache
 fi
 
