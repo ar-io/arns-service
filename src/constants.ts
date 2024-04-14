8bdfbff0d78d44f28b48275ccf7cadadbb2a9d75
dtfiedler <dylan@ar.io>
2024-04-14 13:16:52 -0600

    fix(warp): increase page size for batch page load

--- src/constants.ts
+++ src/constants.ts
@@ -28,12 +28,13 @@ export const ARWEAVE_TX_ID_REGEX = '([a-zA-Z0-9-_s+]{43})';
 export const ARNS_NAME_REGEX = '([a-zA-Z0-9-s+]{1,51})';
 export const SUB_CONTRACT_EVALUATION_TIMEOUT_MS = 10_000; // 10 sec state timeout - non configurable
 export const DEFAULT_REQUEST_TIMEOUT_MS = 180_000;
-export const DEFAULT_STATE_EVALUATION_TIMEOUT_MS = process.env
-  .EVALUATION_TIMEOUT_MS
-  ? +process.env.EVALUATION_TIMEOUT_MS
-  : DEFAULT_REQUEST_TIMEOUT_MS; // 3 min state timeout (should be <= koa request timeout)
+export const DEFAULT_STATE_EVALUATION_TIMEOUT_MS = +(
+  process.env.EVALUATION_TIMEOUT_MS || DEFAULT_REQUEST_TIMEOUT_MS
+); // 3 min state timeout (should be <= koa request timeout)
 export const allowedContractTypes = ['ant'] as const;
-export const DEFAULT_PAGES_PER_BATCH = 50; // loads 5k interactions at a time
+export const DEFAULT_PAGES_PER_BATCH = +(
+  process.env.EVALUATION_PAGES_PER_BATCH || 100
+); // loads 5k interactions at a time
 export const DEFAULT_EVALUATION_OPTIONS: Partial<EvaluationOptions> &
   Required<Pick<EvaluationOptions, 'maxInteractionEvaluationTimeSeconds'>> = {
   maxInteractionEvaluationTimeSeconds: 3600, // one hour
