fe045658ab9d6850dc8ea9471470f513ba0a331d
dtfiedler <dylan@ar.io>
2024-05-09 09:37:40 -0400

    chore(litestream): gracefully exist litestream if replica does not exist

--- litestream/docker-entrypoint.sh
+++ litestream/docker-entrypoint.sh
@@ -6,8 +6,12 @@ ytt --data-values-env TVAL -f /litestream.template.yaml >  /litestream.yml
 
 # restore if needed
 echo "Restoring database from backup..."
-/usr/local/bin/litestream restore -if-db-not-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/state.db
-/usr/local/bin/litestream restore -if-db-not-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/contract.db
+if ! /usr/local/bin/litestream restore -if-db-not-exists -if-replica-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/state.db; then
+    echo "Failed to restore state.db"
+fi
+if ! /usr/local/bin/litestream restore -if-db-not-exists -if-replica-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/contract.db; then
+    echo "Failed to restore contract.db"
+fi
 
 if [[ -n "$WAIT_TIME_SECONDS" ]]; then
     # Sleep for the number of seconds specified in WAIT_TIME_SECONDS
