6a7c18b1840a4bae20e8d3b4ae038bd868eb5eec
dtfiedler <dylan@ar.io>
2024-05-09 08:24:28 -0400

    fix(litestream): update litestream docker file

--- litestream/docker-entrypoint.sh
+++ litestream/docker-entrypoint.sh
@@ -9,5 +9,11 @@ echo "Restoring database from backup..."
 /usr/local/bin/litestream restore -if-db-not-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/state.db
 /usr/local/bin/litestream restore -if-db-not-exists -parallelism 10 -config /litestream.yml $TVAL_LOCAL_DB_PATH/contract.db
 
+if [[ -n "$WAIT_TIME_SECONDS" ]]; then
+    # Sleep for the number of seconds specified in WAIT_TIME_SECONDS
+    echo "Waiting for $WAIT_TIME_SECONDS seconds before starting replication..."
+    sleep $WAIT_TIME_SECONDS
+fi
+
 echo "Starting Litestream for continuous replication..."
 /usr/local/bin/litestream replicate -config /litestream.yml
